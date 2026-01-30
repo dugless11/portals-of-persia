f0d465a925ae01c69f1fba3614341f2be08811b5
reisenlover777 <emit-scarce-velvet@duck.com>
2026-01-30 16:26:33 -0700

    music controller

---
+++ PortalsofPersia-Godot/Scenes/intropt_1.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _ready() -> void:
+	if(MuController.stream != preload("res://assets/mu_ManaTwoPart2_idle.mp3")):
+		MuController.stream = preload("res://assets/mu_ManaTwoPart2_idle.mp3")
+		MuController.play()
--- PortalsofPersia-Godot/Scenes/main.gd
+++ PortalsofPersia-Godot/Scenes/main.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	MuController.stream = preload("res://assets/mu_LateNightRadio_title.mp3")
+	MuController.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,7 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_play_pressed() -> void:
 	#print("press")
-	get_tree().change_scene_to_file("res://Scenes/testScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/intropt1.tscn")
 
 
 func _on_quit_pressed() -> void:
