20c4fe9dc7c476eef901a5261ece84f9ee22f983
dugless11 <112522575+dugless11@users.noreply.github.com>
2026-01-30 10:27:35 -0700

    added main menu and bullet stuff

--- PortalsofPersia-Godot/Objects/bullet.gd
+++ PortalsofPersia-Godot/Objects/bullet.gd
@@ -12,3 +12,9 @@ func _process(delta: float) -> void:
 	if $Line2D.points.size() > trailsize:
 		$Line2D.remove_point(0)
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is TileMapLayer:
+		get_tree().scene
+	pass # Replace with function body.
