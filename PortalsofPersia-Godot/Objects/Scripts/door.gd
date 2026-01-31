c0f1de2ab22b8045233b036eb9ef436029ce5570
BenWahlert <ben.wahlert@gmail.com>
2026-01-30 20:36:51 -0700

    patches

--- PortalsofPersia-Godot/Objects/Scripts/door.gd
+++ PortalsofPersia-Godot/Objects/Scripts/door.gd
@@ -1,29 +1,31 @@
 extends Node2D
 
 @export var button: Node2D
+@export var button2: Node2D
 @export var heightUp = 256
-var downState = true;
-var downPos;
-var upPos = position;
+
+var downPos
+var upPos
+var button1Down = false
+var button2Down = false
 
 func _ready() -> void:
-	button.connect("buttonChanged", buttonChange)
-	downPos = position;
-	upPos = position;
-	upPos.y -= heightUp;
-	
-func buttonChange(isDown):
-	downState = !isDown;
-	if(isDown):
-		print("button is down! opening door ,..,")
-	#	$AnimationPlayer.play("door_up")
-	else:
-		print("buttons up! closing ,.,.")
-	#	$AnimationPlayer.play("door_down")*/
+	button.connect("buttonChanged", Callable(self, "_on_button1_changed"))
+	button2.connect("buttonChanged", Callable(self, "_on_button2_changed"))
+	downPos = position
+	upPos = position
+	upPos.y -= heightUp
+
+func _on_button1_changed(is_down: bool) -> void:
+	button1Down = is_down
+	print("Button 1 is ", button1Down)
+
+func _on_button2_changed(is_down: bool) -> void:
+	button2Down = is_down
+	print("Button 2 is ", button2Down)
 
 func _physics_process(delta: float) -> void:
-	if(downState):
-		position = lerp(position, downPos, 0.03)
-	else:
+	if button1Down and button2Down:
 		position = lerp(position, upPos, 0.03)
-		
+	else:
+		position = lerp(position, downPos, 0.03)
